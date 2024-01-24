0544699587b2046254c600d48455418d345fcd7c
Katerina <ekaterina.tereshko.me@gmail.com>
2024-01-24 23:15:26 +0300

    comments deleted

--- src/App.js
+++ src/App.js
@@ -16,14 +16,12 @@ function App() {
     const getRecipe = async () => {
       const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${wordSubmit}&app_id=${MY_ID}&app_key=${MY_KEY}`);
       const data = await response.json();
-      console.log(data.hits);
       setMyRecipes(data.hits);
     }
     getRecipe()
   }, [wordSubmit]);
 
   const myRecipeSearch = (e) => {
-    console.log(e.target.value);
     setMySearch(e.target.value);
   }
 
